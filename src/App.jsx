1ff86a1b2886b2bbd952c1ca57ed25d2692290fe
YaroslavChmyr <yaroslav.ch72@gmail.com>
2024-09-21 11:23:03 +0300

    Search component is done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import SearchBar from "./components/SearchBar/SearchBar"
+import { useState } from 'react';
+
 function App() {
+  const [query, setQuery] = useState('');
+
+  const onSubmit = query => {
+    setQuery(query);
+    console.log(query);
+  };
 
   return (
     <>
+      <SearchBar onSubmit={onSubmit} />
     </>
   )
 }
